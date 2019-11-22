2b8dd71f87af7e0633a3cf6668593cee0818ee2f
Imranali Sayed <imranalisayed@MacBook-Pro-127.local>
2019-11-21 23:41:04 -0800

    ..

--- gs-spring-boot-docker/initial/src/main/java/hello/Application.java
+++ gs-spring-boot-docker/initial/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Fantasy Stocks™ ~ Kalpi,CV,Rog & Imran";
+		return "Fantasy Stocks™ ~ Kalpi, CV, Rog & Imran";
 	}
 
 	public static void main(String[] args) {
